9672dcce80e5f6b69adafc7541cf1cc8cd71065c
Yenriac <yenriac@yenriac.com>
2022-03-31 13:23:54 +0100

    Alien invasion - Scoring

--- alien_invasion.py
+++ alien_invasion.py
@@ -3,6 +3,7 @@ from time import sleep
 import pygame
 from settings import Settings
 from game_stats import GameStats
+from scoreboard import Scoreboard
 from button import Button
 from ship import Ship
 from bullet import Bullet
@@ -25,8 +26,10 @@ class AlienInvasion:
         pygame.display.set_caption("Alien Invasion")
 
 
-        #create an instance to store game statistics.
+        # Create an instance to store game statistics and
+        # create a scoreboard
         self.stats = GameStats(self)
+        self.sb = Scoreboard(self)
 
         self.ship = Ship(self)
         self.bullets = pygame.sprite.Group()
@@ -134,6 +137,11 @@ class AlienInvasion:
         collisions = pygame.sprite.groupcollide(
                         self.bullets, self.aliens, True, True)
 
+        if collisions:
+            self.stats.score += self.settings.alien_points
+            self.sb.prep_score()
+
+
         if not self.aliens:
             # Destroy existing bullets and create new fleet
             self.bullets.empty()
@@ -211,6 +219,10 @@ class AlienInvasion:
         for bullet in self.bullets.sprites():
             bullet.draw_bullet()
         self.aliens.draw(self.screen)
+
+        # Draw the score information
+        self.sb.show_score()
+
 
         # Draw the Play button if the game is inactive.
         if not self.stats.game_active:
